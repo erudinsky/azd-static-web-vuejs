b28fdd0839a6ba5aeb039af3bd79f66d0d0ee0cc
Evgeny Rudinsky (GitLab) <erudinsky@gitlab.com>
2025-04-11 23:25:06 +0200

    Add sample app

---
+++ vue-app/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <h1>Hello, awesome-azd</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style>
+/* Add any styles here */
+</style>
